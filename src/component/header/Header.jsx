c96ea81d289c7870ef3630dbb8c9422268c431a0
dorsa <dori@gmail.com>
2024-10-08 00:32:42 -0700

    dark mode

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -3,18 +3,22 @@ import { Link } from 'react-router-dom';
 
 // Header Component
 const Header = () => {
+
+  const handleDark = () =>{
+    document.documentElement.classList.toggle('dark')
+  }
   return (
-    <header className="w-full py-4 px-6 bg-white shadow-md flex justify-between items-center rtl">
-      
-      
-       {/* Right Section: Logo */}
-       <div className="flex items-center">
-        <img src="public/logo.png" alt="" className="w-10 h-10" />
+    <header className="flex justify-between items-center w-full py-4 px-6 bg-white shadow-md dark:bg-gray-400 ">
+
+
+      {/* Right Section: Logo */}
+      <div className="flex items-center ">
+        <img src="header icon.png" alt="" className="w-10 h-10" />
         <span className="ml-2 text-xl font-bold"></span>
       </div>
 
       {/* Middle Section: Links */}
-      <nav className='flex ml-auto gap-4'>
+      <nav className='flex ml-auto gap-4 '>
         <Link href='#home' className='text-gray-700 hover:text-blue-500'>
           خانه
         </Link>
@@ -24,26 +28,27 @@ const Header = () => {
         </Link>
 
         <Link href='#about' className='text-gray-700 hover:text-blue-500'>
-         درباره ما
+          درباره ما
         </Link>
 
         <Link href='#contact' className='text-gray-700 hover:text-blue-500'>
-         تماس با ما
+          تماس با ما
         </Link>
-        
+
       </nav>
       {/* Left Section: Buttons */}
-      <div className="flex space-x-4">
-        <button className="bg-blue-500 text-white w-11 h-11 px-4 py-2 rounded-full">
-          
+      <div className="flex gap-4 me-10">
+        <button onClick={handleDark}>
+          <img src="./moon.png" alt="" />
         </button>
-        <button className="bg-gray-500 text-white w-11 h-11 px-4 py-2 rounded-full">
-          
-        </button>
+        <Link>
+          <img src="./profile-user.png" alt="" />
+        </Link>
+
       </div>
 
     </header>
   );
 };
 
-export {Header}
+export { Header }
