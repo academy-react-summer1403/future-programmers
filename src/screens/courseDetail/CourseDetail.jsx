8c22b299f4834e34d95f12753540dd27cf8a7f11
oveiss <oveiss.ershadian@gmail.com>
2024-10-10 21:36:12 +0330

    explanation

--- src/screens/courseDetail/CourseDetail.jsx
+++ src/screens/courseDetail/CourseDetail.jsx
@@ -7,7 +7,7 @@ function CourseDetail() {
     <div className='bg-[#e6e5e5] font-[sans] dark:bg-slate-700'>
       <div className='w-[88%] mx-auto flex'>
         <div className=' relative h-[380px] w-[100%] mt-4 rounded-[30px] bg-[url(public/p.01.png)] bg-no-repeat [background-size:100%_100%]'>
-            <div className="h-[380px] w-[100%] bg-black opacity-95 rounded-[30px] absolute">
+            <div className="h-[100%] w-[100%] bg-black opacity-[0.98] rounded-[30px] absolute">
             <h1 className="text-stone-50  text-[24px] font-black absolute top-[40px] right-[65px]">در لاراول Restful API آموزش کاربردی</h1>
             <p className=' text-gray-400 w-[420px] absolute top-[85px] right-[65px]'> در اینجا یاد می گیرید.ر اینجا یاد می گیرید.ر اینجا یاد می گیرید ر اینجا یاد می گیرید.گیرید.ر اینجا یاد می گیرید.ر ا</p>
             <img src='public/p.01.png' className='w-[450px] h-[310px] shadow-[8px_-13px_35px_-18px_gray] rounded-[25px] absolute right-[770px] top-[30px]'></img>
@@ -20,7 +20,22 @@ function CourseDetail() {
         </div>
       </div>
 
-      <div className='w-[88%] h-[700px] bg-blue-800 mx-auto'></div>
+      <div className=' flex flex-wrap justify-between w-[88%] mx-auto mt-5 relative'>
+        <div className='w-[64%] h-[800px] bg-white rounded-[20px]'>
+          <h1 className='mt-[3%] mr-[3%] text-cyan-700 font-bold text-[20px]'>توضیحات</h1>
+          <p className='mr-[3%] w-[90%] break-words'> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
+          <p className='mr-[3%] w-[90%] break-words'> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
+          <p className='mr-[3%] w-[90%] break-words'> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
+        </div>
+
+        <div className='w-[33%] h-[385px] bg-white rounded-[20px]'>
+          <img></img>
+          <h1></h1>
+          <h4></h4>
+          <p className:''> ssss</p>
+        </div>
+        <div className='w-[33%] h-[385px] bg-white rounded-[20px] absolute left-0 bottom-0'></div>
+      </div>
     </div>
   )
 }
