d3e225a03f47f280852de6fa434618fbc5219b2e
oveiss <oveiss.ershadian@gmail.com>
2024-10-11 00:00:59 +0330

    dark mood coursedetail

--- src/screens/courseDetail/CourseDetail.jsx
+++ src/screens/courseDetail/CourseDetail.jsx
@@ -20,21 +20,23 @@ function CourseDetail() {
         </div>
       </div>
 
-      <div className=' flex flex-wrap justify-between w-[88%] mx-auto mt-5 relative'>
-        <div className='w-[64%] h-[800px] bg-white rounded-[20px]'>
+      <div className=' flex flex-wrap justify-between w-[88%] mx-auto mt-5 relative dark:bg-slate-700'>
+        <div className='w-[64%] h-[800px] bg-white rounded-[20px] dark:bg-slate-500'>
           <h1 className='mt-[3%] mr-[3%] text-cyan-700 font-bold text-[20px]'>توضیحات</h1>
-          <p className='mr-[3%] w-[90%] break-words'> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
-          <p className='mr-[3%] w-[90%] break-words'> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
-          <p className='mr-[3%] w-[90%] break-words'> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
+          <p className='mr-[3%] w-[90%] break-words dark:text-white '> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
+          <p className='mr-[3%] w-[90%] break-words dark:text-white'> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
+          <p className='mr-[3%] w-[90%] break-words dark:text-white'> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
         </div>
 
-        <div className='w-[33%] h-[385px] bg-white rounded-[20px]'>
-          <img></img>
-          <h1></h1>
-          <h4></h4>
-          <p > ssss</p>
+        <div className='w-[33%] h-[385px] bg-white rounded-[20px]  dark:bg-slate-500'>
+          <img className='w-[18%] h-[18%] mt-[20px] mr-[27px] rounded-[50%]' src='public/01.jpg'></img>
+          <h1 className='relative right-[103px] bottom-[63px] text-[20px] text-cyan-700 inline dark:text-white'> آرمان غنی زاده</h1>
+          <h4 className='relative right-[111px] bottom-[60px] text-[14px]'> مدرس دوره</h4>
+          <p className=' w-[86%] border-t-2 border-b-2 mx-auto mt-[-34px] h-[205px]'> ssss</p>
+          <div className='align-center mt-[10px]'><a href='#' className='text-[12px] [background:url(public/external-link.svg)_no-repeat_93%_0%] pr-[30px] mr-[75px]'>مشاهده سایر آموزش های این استاد</a></div>
+          
         </div>
-        <div className='w-[33%] h-[385px] bg-white rounded-[20px] absolute left-0 bottom-0'></div>
+        <div className='w-[33%] h-[385px] bg-white rounded-[20px] absolute left-0 bottom-0  dark:bg-slate-500'></div>
       </div>
     </div>
   )
