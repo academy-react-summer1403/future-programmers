32285c867f8beb1f4759826a8bbc1d8190a10c6f
oveiss <oveiss.ershadian@gmail.com>
2024-10-11 11:30:19 +0330

    doing opinion part

--- src/screens/courseDetail/CourseDetail.jsx
+++ src/screens/courseDetail/CourseDetail.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SimilarCourse from './SimilarCourse'
 
 function CourseDetail() {
     
@@ -20,7 +21,7 @@ function CourseDetail() {
         </div>
       </div>
 
-      <div className=' flex flex-wrap justify-between w-[88%] mx-auto mt-5 relative dark:bg-slate-700'>
+      <div className=' flex flex-wrap justify-between w-[88%] mx-auto mt-5 relative dark:bg-slate-700 pb-[30px]'>
         <div className='w-[64%] h-[800px] bg-white rounded-[20px] dark:bg-slate-500'>
           <h1 className='mt-[3%] mr-[3%] text-cyan-700 font-bold text-[20px] dark:text-black'>توضیحات</h1>
           <p className='mr-[3%] w-[90%] break-words dark:text-white '> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
@@ -34,19 +35,22 @@ function CourseDetail() {
           <h4 className='relative right-[111px] bottom-[60px] text-[14px]'> مدرس دوره</h4>
           <p className=' w-[86%] border-t-2 border-b-2 mx-auto mt-[-34px] h-[205px]'> ssss</p>
           <div className='align-center mt-[10px]'><a href='#' className='text-[12px] [background:url(public/external-link.svg)_no-repeat_93%_0%] pr-[30px] mr-[75px]'>مشاهده سایر آموزش های این استاد</a></div>
-          
         </div>
-        <div className=' w-[33%] h-[385px] bg-white rounded-[20px] absolute left-0 bottom-0  dark:bg-slate-500'>
+
+        <div className=' w-[33%] h-[385px] bg-white rounded-[20px] absolute left-[0] top-[415px]  dark:bg-slate-500'>
           <h1 className='relative right-[30px] top-[30px] text-[20px] text-cyan-700 inline dark:text-white'>دوره های مشابه</h1>
           <div className='relative top-[35px]'>
-            <div className=' border border-gray-600 rounded-[10px] w-[85%] h-[55px] mx-[auto] mt-4'></div>
-            <div className=' border border-gray-600 rounded-[10px] w-[85%] h-[55px] mx-[auto] mt-4'></div>
-            <div className=' border border-gray-600 rounded-[10px] w-[85%] h-[55px] mx-[auto] mt-4'></div>
-            <div className=' border border-gray-600 rounded-[10px] w-[85%] h-[55px] mx-[auto] mt-4'></div>
+          <SimilarCourse />
+          <SimilarCourse />
+          <SimilarCourse />
+          <SimilarCourse />
           </div>
-          
         </div>
+
+        <div className='w-[64%] h-[400px] bg-white mt-[30px] rounded-[20px] dark:bg-slate-500'></div>
       </div>
+
+      
     </div>
   )
 }
---
+++ src/screens/courseDetail/SimilarCourse.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SimilarCourse() {
+  return (
+    <div className='border  border-gray-600 rounded-[10px] w-[85%] h-[55px] mx-[auto] mt-4'>
+        <a href='#' className='flex items-center my-[4px]'>
+            <img className='rounded-[50px] w-[45px] h-[45px] mr-[15px]' src='public/01.jpg'></img>
+            <h5 className='mr-[15px]'>دوره دیگر استاد فلانی</h5>
+        </a>
+    </div>
+  )
+}
+
+export default SimilarCourse
