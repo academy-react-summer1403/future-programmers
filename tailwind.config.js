927db8ff3dd3b568c4e1f0df73da24796ee036d5
dorsa <dori@gmail.com>
2024-10-12 00:30:45 -0700

    edit tailwind confige

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,15 @@ export default {
       'laptop': '1024px',
       // => @media (min-width: 1024px) { ... }
 
+      'sm' : '640px',
+
+      'md' : '768px',
+
+      'lg' : '1024px',
+
+      'xl' : '1280px',
+
+      '2xl' : '1536px'
 
     },
   },
