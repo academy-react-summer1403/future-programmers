8c84718ac38598717fd3ba53049fa24e4829c534
oveiss <oveiss.ershadian@gmail.com>
2024-10-16 13:47:08 +0330

    .

--- src/component/filters/FilterInTop.jsx
+++ src/component/filters/FilterInTop.jsx
@@ -6,13 +6,13 @@ import ReponsiveFilters from "./ReponsiveFilters"
 
 function FilterInTop() {
   return (
-    <div className="drawer drawer-end md:hidden w-[10%]">
+    <div className="drawer drawer-end md:hidden  w-[10%]">
         <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content w-[10%]">
+        <div className="drawer-content w-[10%] ">
             {/* Page content here */}
-            <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary border-0 bg-[white] bg-center w-[100%] h-[100%] bg-[url(public/filter.png)] bg-no-repeat [background-size:19px] dark:bg-[#94a3b8]"></label>
+            <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary hover:bg-[white] border-0 bg-[white] bg-center w-[100%] h-[100%] bg-[url(public/filter.png)] bg-no-repeat [background-size:19px] dark:bg-[#94a3b8]"></label>
         </div>
-        <div className="drawer-side z-[999] max-md:w-[35%] max-sm:w-[72%]">
+        <div className="drawer-side z-[999] max-md:w-[35%] max-sm:w-[70%]">
             <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
             <ul className="menu bg-base-200 text-base-content min-h-full p-4 dark:bg-zinc-700">
                 {/* Sidebar content here */}
