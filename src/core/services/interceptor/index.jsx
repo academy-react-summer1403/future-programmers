26d6fbd0d00e194dfac66ad07cec7a1109c667e9
oveiss <oveiss.ershadian@gmail.com>
2024-10-20 20:39:37 +0330

    .

--- src/core/services/interceptor/index.jsx
+++ src/core/services/interceptor/index.jsx
@@ -11,12 +11,18 @@ const onSuccess = (response) =>{
 }
 const onError = (err) =>{
     console.log(err)
-    if(err.response.status === 404){
-        alert('API not found')
+    if(err.response.status >= 404 && err.response.status < 500){
+        alert('Clinet error:'+ err.response.status)
     }
     return Promise.reject(err);
 }
 
 instance.interceptors.response.use(onSuccess,onError );
+
+
+instance.interceptors. request.use((option) =>{
+    option.headers['MessageTest'] = "Hello world!";
+    return option;
+})
 
 export default instance;
