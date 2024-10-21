4fcbbcd638c0afad28c673e41fbe75853dff6b6c
oveiss <oveiss.ershadian@gmail.com>
2024-10-21 11:05:43 +0330

    change id&htmlfor name in filterInTop

--- src/component/filters/FilterInTop.jsx
+++ src/component/filters/FilterInTop.jsx
@@ -7,10 +7,10 @@ import ReponsiveFilters from "./ReponsiveFilters"
 function FilterInTop() {
   return (
     <div className="drawer drawer-end md:hidden  w-[10%]">
-        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <input id="filter" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content w-[10%] ">
             {/* Page content here */}
-            <label htmlFor="my-drawer-4" className="drawer-button btn btn-primary hover:bg-[white] border-0 bg-[white] bg-center w-[100%] h-[100%] bg-[url(public/filter.png)] bg-no-repeat [background-size:19px] dark:bg-[#556e53]"></label>
+            <label htmlFor="filter" className="drawer-button btn btn-primary hover:bg-[white] border-0 bg-[white] bg-center w-[100%] h-[100%] bg-[url(public/filter.png)] bg-no-repeat [background-size:19px] dark:bg-[#556e53]"></label>
         </div>
         <div className="drawer-side z-[999] max-md:w-[35%] max-sm:w-[268px]">
             <label htmlFor="my-drawer-4" aria-label="close sidebar" className="drawer-overlay"></label>
