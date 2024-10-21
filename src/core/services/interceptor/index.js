4bc45180e4961dd2a8c1e79fde0b0d8e42827bff
oveiss <oveiss.ershadian@gmail.com>
2024-10-21 20:15:12 +0330

    add interceptor

--- src/core/services/interceptor/index.js
+++ src/core/services/interceptor/index.js
@@ -1,49 +1,26 @@
+
 import axios from "axios";
-import { json } from "react-router-dom";
-import { getItem } from "../common/storage.services";
-
-
-
-
-const baseURL = import.meta.env.VITE_BASE_URL
+const baseURL = import.meta.env.VITE_BASE_URL;
 
 const instance = axios.create({
-    baseURL: baseURL,
+  baseURL: baseURL,
 });
 
 const onSuccess = (response) => {
-    return response.data
-}
+//   console.log("response", response);
+
+  return response.data;
+};
 
 const onError = (err) => {
-    // console.log(err);
-
-    // if(err.response.status === 401){
-    //     // clearStorage()
-    //     removeItem('token');
-    //     window.location.pathname = '/' // or '/login'
-    // }
-
-    // if(err.response.status >= 400 && err.response.status < 500){
-    //     // alert("Client request error: " + err.response.status);
-    // }
-
-    return Promise.reject(err);
-}
+  console.log(err);
+  return Promise.reject(err);
+};
 
 instance.interceptors.response.use(onSuccess, onError);
 
-instance.interceptors.request.use(opt => {
-
-    //const user = useSelector(state => state.user)
-
-    const token = getItem("token") ? getItem("token") : null;
-
-
-    //  opt.headers['MessageTest'] = "Hello World"; 
-    //  opt.headers['Content-Type'] = "application/json";
-    if (token) opt.headers.Authorization = 'Bearer ' + token;
-    return opt
-})
+instance.interceptors.request.use((opt) => {
+  return opt;
+});
 
 export default instance;
--- src/screens/courses/Courses.jsx
+++ src/screens/courses/Courses.jsx
@@ -5,16 +5,35 @@ import CourseCard from '../../component/CourseCard/CourseCard';
 import TopCategory from '../../component/topCategory/TopCategory';
 import Filters from '../../component/filters/UsualFilters';
 import FilterInTop from '../../component/filters/FilterInTop';
+// import http from "../../core/services/interceptor"
+import { useEffect, useState } from 'react';
+import { getlist } from '../../core/services/api/course';
+import pic from '../../../public/item1.png'
 // import UseDarkMood from './UseDarkMood';
 
 
 const Courses = () => {
-  const Course=[
-    {image:'public/01.jpg', topic:'آموزش ری اکت 1', explain:'آموزش می دهیم. آموزش نمی دهیم', teacher:'استاد یک',time:'12:12:12', price:'رایگان!'},
-    {image:'public/01.jpg',topic:'آموزش ری اکت 2', explain:'آموزش می دهیم. آموزش می دهیم', teacher:'استاد دو',time:'12:12:12', price:'رایگان!'},
-    {image:'public/01.jpg',topic:'آموزش ری اکت 3', explain:'آموزش می دهیم. آموزش نمی دهیم', teacher:'استاد سه',time:'12:12:12', price:'رایگان!'},
-    {image:'public/01.jpg',topic:'آموزش ری اکت 3', explain:'آموزش می دهیم. آموزش نمی دهیم', teacher:'استاد سه',time:'12:12:12', price:'رایگان!'}
-]
+//   const Course=[
+//     {image:'public/01.jpg', topic:'آموزش ری اکت 1', explain:'آموزش می دهیم. آموزش نمی دهیم', teacher:'استاد یک',time:'12:12:12', price:'رایگان!'},
+//     {image:'public/01.jpg',topic:'آموزش ری اکت 2', explain:'آموزش می دهیم. آموزش می دهیم', teacher:'استاد دو',time:'12:12:12', price:'رایگان!'},
+//     {image:'public/01.jpg',topic:'آموزش ری اکت 3', explain:'آموزش می دهیم. آموزش نمی دهیم', teacher:'استاد سه',time:'12:12:12', price:'رایگان!'},
+//     {image:'public/01.jpg',topic:'آموزش ری اکت 3', explain:'آموزش می دهیم. آموزش نمی دهیم', teacher:'استاد سه',time:'12:12:12', price:'رایگان!'}
+// ]
+const [Course, setCourse] = useState([])
+// console.log('Course',Course)
+
+const getAllCoursesList = async ()=>{
+    try {
+        const result = await getlist()
+        setCourse(result.courseFilterDtos)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+useEffect(()=>{
+    getAllCoursesList() 
+},[])
 
 return (
 <div className='bg-[#e6e5e5] font-[sans] dark:bg-[#152a38]'>
@@ -28,7 +47,7 @@ return (
             <Formik initialValues={{SearchCourse:'', New:'',BestSelling:'',Expensive:'',Cheap:'' , sort:null }}>
                 <Form  className='w-[100%] h-[115px] md:max-lg:h-[100%] max-md:h-[100%] bg-white rounded-[20px] flex items-center md:max-lg:gap-0 gap-4 gap-0 dark:bg-[#29435c]'>
             
-                    <Field className='p-4 md:max-lg:p-2 w-[25%] md:max-lg:w-[27%] max-md:w-[50%] max-md:mx max-sm:w-[70%] mr-[1%] max-sm:mr-[3%] bg-[url(public/search.svg)] [background-position-y:center] max-sm:[background-position-x:3%] bg-no-repeat text-base max-sm:p-[9px] md:max-lg:text-[11px] font-bold [background-size:35px] md:max-lg:[background-size:25px] max-sm:[background-size:25px] max-sm:text-[13px] [center] shadow-inner shadow-slate-400 rounded-xl dark:bg-[#556e53] dark:placeholder-[#d1d4c9]' type='text' placeholder='جستجوی دوره' name='SearchCourse'  />
+                    <Field  className='p-4 md:max-lg:p-2 w-[25%] md:max-lg:w-[27%] max-md:w-[50%] max-md:mx max-sm:w-[70%] mr-[1%] max-sm:mr-[3%] bg-[url(public/search.svg)] [background-position-y:center] max-sm:[background-position-x:3%] bg-no-repeat text-base max-sm:p-[9px] md:max-lg:text-[11px] font-bold [background-size:35px] md:max-lg:[background-size:25px] max-sm:[background-size:25px] max-sm:text-[13px] [center] shadow-inner shadow-slate-400 rounded-xl dark:bg-[#556e53] dark:placeholder-[#d1d4c9]' type='text' placeholder='جستجوی دوره' name='SearchCourse'  />
                     <TopCategory />
 
                     {/* <Sorting /> */}
@@ -45,12 +64,12 @@ return (
                     return(
                         <CourseCard 
                         key={index}
-                        image={item.image}
-                        topic={item.topic} 
-                        explain={item.explain} 
-                        teacher={item.teacher} 
-                        time={item.time} 
-                        price={item.price} />  
+                        image={item.tumbImageAddress??pic}
+                        topic={item.title} 
+                        explain={item.describe} 
+                        teacher={item.teacherName} 
+                        time={item.lastUpdate} 
+                        price={item.cost} />  
                     );   
                 })}             
             </div>
