e1043fe95e3c4cb270d98453a5491aa0c552f1cc
oveiss <oveiss.ershadian@gmail.com>
2024-10-24 22:51:28 +0330

    change searchbox

--- src/component/SearchBoxInCourses/SearchBox.jsx
+++ src/component/SearchBoxInCourses/SearchBox.jsx
@@ -1,10 +1,12 @@
 import { Field, Form, Formik } from 'formik'
+import Search from '../../../public/search.svg';  
 
 function SearchBox() {
   return (
     <Formik initialValues={{SearchCourse:'', sort:null }}>
-        <Form>
-            <Field className='lg:p-4 md:max-lg:p-2 mr-[3%] bg-[url(public/search.svg)] [background-position-y:center] max-sm:[background-position-x:3%] bg-no-repeat text-base max-md:p-[9px] md:max-lg:text-[11px] font-bold [background-size:35px] md:max-lg:[background-size:25px] max-sm:[background-size:25px] max-sm:text-[13px] [center] shadow-inner shadow-slate-400 rounded-xl dark:bg-[#556e53] dark:placeholder-[#d1d4c9]' type='text' placeholder='جستجوی دوره' name='SearchCourse'  />
+        <Form className='flex mr-[1%] shadow-inner shadow-slate-400 rounded-xl dark:bg-[#556e53]'>
+            <Field className=' h-[35px] w-[220px] max-lg:w-[175px] mr-[3%] my-auto max-lg:text-[12px] font-bold max-sm:text-[13px] [outline:none] dark:bg-[#556e53] dark:placeholder-[#d1d4c9]' type='text' placeholder='جستجوی دوره' name='SearchCourse' />
+            <button className='bg-[url(public/search.svg)] dark:bg-[url(public/dark.search.svg)] [background-size:35px] w-[60px] h-[48px] m-auto bg-no-repeat [background-position:50%_50%] hover:scale-110 duration-200 '></button>
         </Form>
     </Formik>
   )
