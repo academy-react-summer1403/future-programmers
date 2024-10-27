4453acdccbc3167e6e190a55d17830e09bfd8d83
oveiss <oveiss.ershadian@gmail.com>
2024-10-27 15:32:54 +0330

    .

--- src/component/landingComponent/LastNews/LastNews.jsx
+++ src/component/landingComponent/LastNews/LastNews.jsx
@@ -23,7 +23,6 @@ const LastNews = () => {
                 <h2 className="text-2xl font-bold text-blue-400">آخرین اخبار</h2>
 
                 <div className="flex flex-wrap gap-2 justify-evenly mt-10 ">
-                
                     {NewsList?.map((item, index) => {
                         return (
                             <div key={index} className='bg-white rounded-3xl w-[90%] mt-8 md:w-[40%] lg:w-[20%] h-96  '>
