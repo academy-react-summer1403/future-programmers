3e3055c33c04c20c9d5dd1bce93d0ff908f88d92
oveiss <oveiss.ershadian@gmail.com>
2024-10-29 11:52:00 +0330

    correct font in header

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
   };
   
   return (
-    <div className=" text-[sans] bg-[#f9f9f9] flex justify-between items-center w-full py-4 px-6  shadow-2xl dark:bg-[#152a38] ">
+    <div className=" font-[sans] bg-[#f9f9f9] flex justify-between items-center w-full py-4 px-6  shadow-2xl dark:bg-[#152a38] ">
       <div className="flex items-center  ">
         <img src="header icon.png" alt="" className="w-14 h-14" />
         <span className=" ml-2 text-xl font-bold"></span>
