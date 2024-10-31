d71ea698a455afb6a5cbd2f993e8bbecd0096cd4
oveiss <oveiss.ershadian@gmail.com>
2024-10-31 10:44:42 +0330

    .

--- src/screens/courseDetail/CourseDetail.jsx
+++ src/screens/courseDetail/CourseDetail.jsx
@@ -33,17 +33,18 @@ function CourseDetail() {
           <h1 className='mt-[3%] mr-[3%] my-3 text-cyan-700 font-bold text-[20px] max-md:text-[17px] dark:text-[#d1d4c9]'>توضیحات</h1>
           <p className='mr-[3%] w-[95%] break-words max-md:text-[15px] dark:text-[#d1d4c9] '> jfjfjfjfjfjfj   jfjfjfjfjfjfj The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis. The longest word in any of the major English language dictionaries is pneumonoultramicroscopicsilicovolcanoconiosis, a word that refers to a lung disease contracted from the inhalation of very fine silica particles, specifically from a volcano; medically, it is the same as silicosis.</p>
            
-          <div className='w-[100%] mx-[auto] pb-5'>
-            <div className='w-[100%] h-[400px] bg-white mt-[30px] max-md:mt-0 rounded-[20px] max-md:w-[100%] md:max-lg:w-[100%] dark:bg-[#29435c]'>
-              <div className='w-[85%] h-[90%] my-[20px]  mx-auto'>
-                <h1 className='text-[25px] text-cyan-700 inline dark:text-[#d1d4c9]'>نظرات</h1>
-                <div className='border-[2px] border-cyan-700 dark:border-[#556e53] rounded-[20px] h-[250px] mt-[5px]'>
-                  <textarea className='w-[99%] h-[240px] mr-[6px] mt-[4px] rounded-[20px] [outline:none] max-md:w-[97%] dark:bg-[#29435c] dark:text-[#d1d4c9]' >نظر خود را اینجا بنویسید</textarea>
+           {/* change and make better this part. */}
+           {/* <div className='w-[100%] mx-[auto] pb-5'>
+              <div className='w-[100%] h-[400px] bg-white mt-[30px] max-md:mt-0 rounded-[20px] max-md:w-[100%] md:max-lg:w-[100%] dark:bg-[#29435c]'>
+                <div className='w-[85%] h-[90%] my-[20px]  mx-auto'>
+                  <h1 className='text-[25px] text-cyan-700 inline dark:text-[#d1d4c9]'>نظرات</h1>
+                  <div className='border-[2px] border-cyan-700 dark:border-[#556e53] rounded-[20px] h-[250px] mt-[5px]'>
+                    <textarea className='w-[99%] h-[240px] mr-[6px] mt-[4px] rounded-[20px] [outline:none] max-md:w-[97%] dark:bg-[#29435c] dark:text-[#d1d4c9]' >نظر خود را اینجا بنویسید</textarea>
+                  </div>
+                  <button className='bg-cyan-700 mt-[10px]  py-3 px-4 rounded-[10px] text-white dark:bg-[#556e53]'>ارسال نظر</button>
                 </div>
-                <button className='bg-cyan-700 mt-[10px]  py-3 px-4 rounded-[10px] text-white dark:bg-[#556e53]'>ارسال نظر</button>
               </div>
-            </div>
-          </div>
+            </div> */}
         </div>
 
         <div className='flex flex-wrap h-[800px] max-lg:h-[400px] max-md:h-[800px] w-[33%] gap-y-5 max-md:w-[100%] md:max-lg:w-[100%] md:max-lg:justify-between md:max-lg:mt-5'>
@@ -69,7 +70,10 @@ function CourseDetail() {
               })}
             </div>
           </div>
+           
         </div>
+
+        
       </div>
 
       
