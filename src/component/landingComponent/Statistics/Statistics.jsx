92222f83b2159096b7c685e3a0bb02a332d22df8
oveiss <oveiss.ershadian@gmail.com>
2024-11-01 22:57:25 +0330

    .

--- src/component/landingComponent/Statistics/Statistics.jsx
+++ src/component/landingComponent/Statistics/Statistics.jsx
@@ -16,9 +16,9 @@ function Statistics() {
     }, []);
 
     return (
-        <div className=" mx-auto px-4 sm:h-44 lg:h-96 sm:flex flex-nowrap ">
-            <img className="relative h-[140%] w-[50%] right-[-3%] z-0 max-sm:hidden" src={pic} />
-            <div className="relative xl:left-[50px] sm:max-md:left-[20px] sm:max-md:w-[430px] overflow-hidden text-center md:w-1/2 content-center flex flex-wrap justify-between max-sm:h-9 max-sm:rounded-xl max-sm:shadow-md">
+        <div className=" mx-auto px-4 sm:h-44 lg:h-96 ">
+            {/* <img className="relative h-[140%] w-[50%] right-[-3%] z-0 max-sm:hidden" src={pic} /> */}
+            <div className=" xl:left-[50px] sm:max-md:left-[20px] sm:max-md:w-[430px] overflow-hidden text-center content-center flex flex-wrap justify-between max-sm:h-9 max-sm:rounded-xl max-sm:shadow-md">
                     <div className="flex flex-wrap justify-between max-sm:justify-between basis-full max-sm:text-[10px] max-md:text-[12px] dark:bg-[#29435c]">
                         <div className="w-[20%] max-md:w-[24%] dark:text-[#d1d4c9]">
                             <h2 className='text-[#436e8e]'>{landingInfo?.teacherCount }</h2>
