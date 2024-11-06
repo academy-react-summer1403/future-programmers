283274feba67c02137483840fafb37dc3ce64894
oveiss <oveiss.ershadian@gmail.com>
2024-11-06 23:57:33 +0330

    .

--- src/core/services/api/coursedetail.js
+++ src/core/services/api/coursedetail.js
@@ -1,10 +1,12 @@
 import http from '../interceptor'
 
-export const getCourseDetail = async()=>{
+export const getCourseDetail = async(id)=>{
     try {
         
-        const result = await http.get('/Home/GetCoursesWithPagination')
+        const result = await http.get(`/Home/GetCourseDetails?CourseId=${id}`)
+        // console.log('qqq',id)
         return result
+        
         
     } catch (error) {
         console.log(error)
