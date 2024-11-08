44b6b20a7b9575d29019b54cab936678ef21ef5b
oveiss <oveiss.ershadian@gmail.com>
2024-11-08 15:53:58 +0330

    .

--- src/screens/courseDetail/CourseDetail.jsx
+++ src/screens/courseDetail/CourseDetail.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
             <div className="h-[100%] w-[100%] bg-black opacity-[0.98]  max-sm:opacity-[0.91] rounded-[30px]">
               <h1 className="text-stone-50  text-[24px] max-md:text-[10px] max-sm:text-[11px] max-md:right-[30px] md:max-lg:text-[13px] md:max-lg:right-[38px] font-black absolute top-[40px] right-[65px]">در لاراول Restful API آموزش کاربردی</h1>
               <p className=' text-gray-400 w-[420px] absolute top-[85px] right-[65px] max-md:right-[30px] max-md:w-[205px] max-md:top-[56px] max-md:text-[8px] md:max-lg:top-[60px] md:max-lg:text-[10px] md:max-lg:right-[38px] md:max-lg:w-[251px] max-sm:text-[10px] max-sm:w-[250px] max-sm:top-[60px] max-sm:text-slate-200'> در اینجا یاد می گیرید.ر اینجا یاد می گیرید.ر اینجا یاد می گیرید ر اینجا یاد می گیرید.گیرید.ر اینجا یاد می گیرید.ر ا</p>
-              <img src={detail.imageAddress??pic} className='w-[450px] h-[310px] shadow-[8px_-13px_35px_-18px_gray] rounded-[25px] absolute right-[770px] top-[30px]  max-md:right-[334px] max-md:w-[44%] max-md:h-[76%] md:max-lg:right-[406px] md:max-lg:h-[76%] md:max-lg:w-[43%]  max-sm:hidden'></img>
+              <img src={detail?.imageAddress??pic} className='w-[450px] h-[310px] shadow-[8px_-13px_35px_-18px_gray] rounded-[25px] absolute right-[770px] top-[30px]  max-md:right-[334px] max-md:w-[44%] max-md:h-[76%] md:max-lg:right-[406px] md:max-lg:h-[76%] md:max-lg:w-[43%]  max-sm:hidden'></img>
               <h3 className=' text-gray-400 line-through absolute top-[220px] right-[625px] max-md:text-[11px] max-md:right-[240px] max-md:top-[164px] md:max-lg:text-[12px] md:max-lg:top-[175px] md:max-lg:right-[295px]'> تومان 500,000</h3>
               <h2 className=' text-white	text-[22px] font-bold	absolute top-[250px] right-[580px] max-md:text-[13px] max-md:top-[180px] max-md:right-[221px] md:max-lg:text-[14px] md:max-lg:top-[200px] md:max-lg:right-[273px]'>تومان 400,000</h2>
               <button type='Submit' className='bg-cyan-700 text-[white] py-3 px-4 rounded-[10px] absolute top-[242px] right-[70px] max-md:top-[165px] max-md:p-[7px] max-md:text-[11px] max-md:right-[24px] md:max-lg:text-[12px] md:max-lg:right-[38px] md:max-lg:top-[190px] dark:bg-[#556e53]'>خرید نقدی دوره</button>
@@ -59,21 +59,21 @@ useEffect(() => {
 
       <div className=' flex flex-wrap justify-between w-[88%] mx-auto mt-5 max-sm:mt-3 pb-[30px] overflow-hidden dark:bg-[#152a38]'>
       {/* <CourseExplain/> */}
-        <CourseExplain describe={detail.describe}/>
+        <CourseExplain describe={detail?.describe}/>
         
         <div className='flex flex-wrap max-md:h-fit w-[33%] gap-y-2 max-lg:w-[100%] md:max-lg:justify-between md:max-lg:mt-5'>
             <CourseInformation 
-            teacherName={detail.teacherName}
-            cost={detail.cost}
-            // techs={detail.techs[0]+', '+detail.techs[1]}
-            courseLevelName={detail.courseLevelName}
-            capacity={detail.capacity}
-            courseStatusName={detail.courseStatusName}
-            commentCount={detail.commentCount}
-            currentRate={detail.currentRate}
-            // insertDate={detail.insertDate.toString().slice(0,10)}
-            // startTime={detail.startTime.toString().slice(0,10)}
-            // endTime={detail.endTime.toString().slice(0,10)}
+            teacherName={detail?.teacherName}
+            cost={detail?.cost}
+            // techs={detail?.techs}
+            courseLevelName={detail?.courseLevelName}
+            capacity={detail?.capacity}
+            courseStatusName={detail?.courseStatusName}
+            commentCount={detail?.commentCount}
+            currentRate={detail?.currentRate}
+            insertDate={detail?.insertDate?.toString().slice(0,10)}
+            startTime={detail?.startTime?.toString().slice(0,10)}
+            endTime={detail?.endTime?.toString().slice(0,10)}
             />
 
 
