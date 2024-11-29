cd0a4631ae841b9466c243d379d9d235f61462ec
oveiss <oveiss.ershadian@gmail.com>
2024-11-29 15:22:35 +0330

    .

--- src/component/landingComponent/LastNews/newsCard.jsx
+++ src/component/landingComponent/LastNews/newsCard.jsx
@@ -1,16 +1,51 @@
 import { Link } from "react-router-dom"
+import liked from '../../../../public/Liked.red.png'
+import notLiked from '../../../../public/NotLiked.png'
+import disliked from '../../../../public/dislike.red.png'
+import notDisLiked from '../../../../public/dislike.empty.png'
+import star from '../../../../public/star.png'
+import save from '../../../../public/save.png'
+import saved from '../../../../public/saved.png'
+import eye from '../../../../public/eye.png'
 
 
-function NewsCard({image, newsTitle, newsText, writer,id}) {
+function NewsCard({image, currentDissLikeCount, currentUserIsDissLike, currentUserIsLike, newsTitle, currentView, isCurrentUserFavorite, miniDescribe, addUserFullName, newsCatregoryName, currentLikeCount, insertDate, id}) {
   return (
-    <div className='bg-white rounded-3xl flex h-[250px] max-lg:h-[210px] max-md:h-40 w-[49%] max-sm:w-[100%] dark:bg-[#29435c] '>
-        <img src={image} alt="" className=' rounded-2xl h-[87%] lg:w-[38%] sm:w-[43%] max-sm:w-[26%] my-auto mr-[4%]' />
-        <div className='h-[87%] lg:w-[60%] sm:w-[55%] max-sm:w-[72%] my-auto mr-[3%]'>
-          <Link to={'/NewsDetail/'+id} className='font-extrabold text-violet-800 dark:text-[#c7e7b7] max-lg:text-[13px] max-md:text-[11px] max-md:font-normal'>{newsTitle}</Link>
-          <p className='mt-2 indent-2 w-[95%] h-36 max-lg:h-32 max-md:h-[92px] text-[14px] max-lg:text-[12px] max-md:text-[10px] max-sm:text-[9px] dark:text-[#d1d4c9] overflow-hidden'>{newsText}</p>
-          <h3 className='text-xs font-semibold max-lg:font-normal  max-md:text-[10px] mt-[5%] max-lg:mt-[4%] max-md:mt-0 flex dark:text-[#c7e7b7]'>نویسنده:<h3 className="mr-1 text-red-700 dark:text-[#e19f9f]" > {writer}</h3></h3>
-        </div>                           
-    </div>
+    <div className=" relative w-[24%] max-sm:w-[95%] max-md:w-[47%] md:max-lg:w-[24%] pb-2 bg-white rounded-[20px] dark:dark:bg-[#29435c] dark:text-[#d1d4c9]">
+        <img src={image} className="w-[92%] h-36 md:max-lg:h-[42%] mx-auto border border-[gray] relative bottom-6 rounded-3xl"></img>
+        <h1 className="text-[14px] max-md:text-[12px] max-lg:text-[9px] font-bold pr-4 relative bottom-5 dark:text-[#d1d4c9]">{newsTitle}</h1>
+        <div className="relative flex justify-between bottom-3 pr-4 pl-3 max-md:text-[11px] text-[13px] md:max-lg:text-[10px] text-[#0004ff] dark:text-[#d1d4c9]">
+          <h6 className="text-red-700 dark:text-[#e19f9f]">{addUserFullName}</h6>
+          <div className='flex items-center'>
+            <span className='text-xs max-lg:text-[9px] pl-2'>{currentView}</span>
+            <img src={eye} className='w-5 max-lg:w-4 h-5 max-lg:h-4' alt=""></img>
+          </div>
+        </div>
+        <div className="flex gap-3">
+          <span className="relative bottom-2 bg-slate-400 mr-4 px-1 pt-1 pb-2 bg-opacity-60 rounded-md text-[12px] md:max-lg:text-[10px] dark:bg-opacity-50 dark:dark:bg-[#556e53]">{newsCatregoryName}</span>
+          <img src={isCurrentUserFavorite === true ? saved : save} className='w-5 max-lg:w-4 h-5 max-lg:h-4' alt=""></img>
+        </div>
+        <p className="w-[95%] indent-[8px] h-9 max-lg:h-6 max-md:h-8 pr-2 relative  max-sm:text-[11px]  max-md:text-[10px]  mx-auto text-[12px] md:max-lg:text-[8px]  overflow-hidden dark:text-[#d1d4c9]">{miniDescribe}</p>
+        
+        <div className="flex justify-between mt-1 pl-3">
+        <div className="flex pr-4 gap-2">
+          <div className=''>
+            <img src={currentUserIsLike === true ? liked : notLiked} className='w-5 max-lg:w-4 h-5 max-lg:h-4 ' alt=""></img>
+            <span className='text-xs max-lg:text-[9px] mt-2  pr-[2px]'>{currentLikeCount}</span>
+          </div>
+          <div className=''>
+            <img src={currentUserIsDissLike === true ? disliked : notDisLiked} className='w-5 max-lg:w-4 h-5 max-lg:h-4' alt=""></img>
+            <span className='text-[12px] max-lg:text-[9px] mt-2  pr-[2px]'>{currentDissLikeCount}</span>
+          </div>
+        </div>
+          <Link to={'/NewsDetail/'+id} className="text-[12px] sm:text-[11px] h-9 pt-2 px-2 my-auto rounded-xl bg-slate-400 dark:dark:bg-[#556e53] dark:text-[#d1d4c9]">جزئیات خبر</Link>
+        </div>
+        <div  className="border-t border-slate-400 flex items-center justify-between w-[90%] mx-auto pt-2 max-lg:pt-1">
+            <span className="text-[13px] md:max-lg:text-[11px]"> تاریخ خبر</span>
+            <span className="   max-md:text-[12px] max-lg:text-[11px] text-[13px] max-xl:text-[13px] text-red-700 dark:text-[#d1d4c9]" >{insertDate}  </span>
+        </div>
+        {/* <span className="absolute mt-6 md:max-lg:mt-2 left-[2%] max-md:left-[3%]  max-md:text-[11px]  max-lg:pr-[4px] bg-slate-400 rounded-2xl text-[14px] max-xl:text-[13px] max-lg:text-[10px] p-[2px] pr-1 pl-5 md:max-lg:pl-4 bg-[url(public/timer.png)] bg-no-repeat [background-position:2%_50%] max-lg:[background-position:4%_50%] [background-size:18px] md:max-lg:[background-size:14px] dark:dark:bg-[#556e53] dark:text-[#d1d4c9]">{time}</span> */}
+        </div> 
   )
 }
 
