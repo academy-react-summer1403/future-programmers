b1d14ca60a735ed761d677e7175144d45c58319c
oveiss <oveiss.ershadian@gmail.com>
2024-12-04 16:43:44 +0330

    .

--- src/screens/page news/CardNews.jsx
+++ src/screens/page news/CardNews.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import { getCardNews } from "../../core/services/api/news";
 import Page from "./Page";
 import PictureBox from "./PictureBox";
-import CardPage2 from "./CardPage2";
+// import CardPage2 from "./CardPage2";
 import pic from "../../../public/newspaper.png";
 
 const CardNews = () => {
   const [cardNews, setCardNews] = useState([]);
   console.log(cardNews);
-  const News = cardNews.slice(0, 4);
+  const topNews = cardNews.slice(0, 4);
+  const bottomNews = cardNews.slice(4, 8);
 
+  
   const getAllCardNews = async () => {
     try {
       const result = await getCardNews();
@@ -24,19 +26,26 @@ const CardNews = () => {
     getAllCardNews();
   });
   return (
-    <div className=" shadow-lg w-[95%] m-auto bg-[#ffff] rounded-lg dark:bg-[#152a38]  max-lg:w-[95%] max-md:w-[80%]  max-md: max-sm:w-[80%]">
-      <div className="flex flex-wrap w-[100%] dark:bg-[#29435c] dark:text-[#d1d4c9]">
-        <div className="flex flex-wrap w-[95%] mx-auto max-sm:gap-y-3 justify-between dark:bg-[#29435c] dark:text-white">
-          {News.map((item, index) => {
+    <div className=" shadow-lg w-[95%] m-auto bg-[#f3f4f6] rounded-lg dark:bg-[#152a38]  max-lg:w-[95%] max-md:w-[80%]  max-md: max-sm:w-[80%]">
+      <div className="flex flex-wrap w-[100%] dark:bg-[#152a38] dark:text-[#d1d4c9]">
+        <div className="flex flex-wrap w-[95%] mx-auto max-sm:gap-y-3 justify-between dark:bg-[#152a38] dark:text-white">
+          {topNews.map((item, index) => {
             return (
               <Page
-                key={index}
-                title={item.title}
-                miniDescribe={item.miniDescribe}
-                currentDissLikeCount={item.currentDissLikeCount}
-                currentLikeCount={item.currentLikeCount}
-                image={item.currentImageAddressTumb ?? pic}
-                id={item.id}
+              key={index}
+              image={item.currentImageAddressTumb??pic}
+              newsTitle={item.title}
+              miniDescribe={item.miniDescribe}
+              addUserFullName={item.addUserFullName}
+              currentDissLikeCount={item?.currentDissLikeCount}
+              currentUserIsLike={item?.currentUserIsLike}
+              currentUserIsDissLike={item?.currentUserIsDissLike}
+              newsCatregoryName={item?.newsCatregoryName}
+              currentView={item?.currentView}
+              isCurrentUserFavorite={item?.isCurrentUserFavorite}
+              currentLikeCount={item?.currentLikeCount}
+              insertDate={item?.insertDate?.toString()?.slice(0,10)}
+              id={item.id} 
               />
             );
           })}
@@ -44,18 +53,25 @@ const CardNews = () => {
         </div>
       </div>
 
-      <div className="bg-[#fff] w-[100%] dark:bg-[#29435c] dark:text-[#d1d4c9]  ">
+      <div className="bg-[#f3f4f6] w-[100%] dark:bg-[#29435c] md:mt-8 dark:text-[#d1d4c9]  ">
         <div className=" flex flex-wrap w-[95%] mx-auto max-sm:gap-y-3 justify-between dark:bg-[#29435c] dark:text-white ">
-          {News.map((item, index) => {
+          {bottomNews.map((item, index) => {
             return (
-              <CardPage2
-                key={index}
-                title={item.title}
-                miniDescribe={item.miniDescribe}
-                currentDissLikeCount={item.currentDissLikeCount}
-                currentLikeCount={item.currentLikeCount}
-                image={item.currentImageAddressTumb ?? pic}
-                id={item.id}
+              <Page
+              key={index}
+              image={item.currentImageAddressTumb??pic}
+              newsTitle={item.title}
+              miniDescribe={item.miniDescribe}
+              addUserFullName={item.addUserFullName}
+              currentDissLikeCount={item?.currentDissLikeCount}
+              currentUserIsLike={item?.currentUserIsLike}
+              currentUserIsDissLike={item?.currentUserIsDissLike}
+              newsCatregoryName={item?.newsCatregoryName}
+              currentView={item?.currentView}
+              isCurrentUserFavorite={item?.isCurrentUserFavorite}
+              currentLikeCount={item?.currentLikeCount}
+              insertDate={item?.insertDate?.toString()?.slice(0,10)}
+              id={item.id} 
               />
             );
           })}
--- src/screens/page news/Page.jsx
+++ src/screens/page news/Page.jsx
@@ -1,53 +1,84 @@
-// <<<<<<< HEAD
-// import { Link } from "react-router-dom";
-
-// const Page = ({title,desc, id}) => {
-//   return (
-//     <Link to={'/NewsDetail/'+id} className=" mt-3 bg-white rounded-lg w-[23%] dark:bg-[#152a38] max-lg:w-[45%]  max-sm:w-[100%] max-sm:mt-2 shadow-lg ">
-//         <div className="h-[40%] w-[90%]  mx-auto pt-3 ">
-//             <img src="./newspaper.png" alt="" className=" h-[100%] w-[100%]   " />
-//         </div>
-//         <h1 className="pr-5 mt-2"> {title} </h1>
-//         <p className="  mx-auto pr-3 mt-1 w-[95%] h-[50%] overflow-hidden"> {desc} </p>
-//     </Link>
-// =======
-import { Link, NavLink } from "react-router-dom";
 import { TiHeartOutline } from "react-icons/ti";
 import { AiOutlineLike } from "react-icons/ai";
 import { AiOutlineDislike } from "react-icons/ai";
 
-const Page = ({title,miniDescribe,currentDissLikeCount,currentLikeCount, image,id}) => {
+import { Link } from "react-router-dom"
+import liked from '../../../public/Liked.red.png'
+import notLiked from '../../../public/NotLiked.png'
+import disliked from '../../../public/dislike.red.png'
+import notDisLiked from '../../../public/dislike.empty.png'
+import star from '../../../public/star.png'
+import save from '../../../public/save.png'
+import saved from '../../../public/saved.png'
+import eye from '../../../public/eye.png'
+
+const Page = ({image, currentDissLikeCount, currentUserIsDissLike, currentUserIsLike, newsTitle, currentView, isCurrentUserFavorite, miniDescribe, addUserFullName, newsCatregoryName, currentLikeCount, insertDate, id}) => {
     console.log(id)
 return (
     
-    <div className=" mt-3 bg-white rounded-lg w-[23%] dark:bg-[#152a38] max-lg:w-[45%]  max-sm:w-[100%] max-sm:mt-2 shadow-lg">
-        
-        <div className="h-[40%] w-[90%]  mx-auto pt-3 ">
-            <img src={image} alt="" className=" h-[100%] w-[100%] rounded-[20px]" />
+    <div className=" relative w-[24%] max-md:mt-10 max-sm:w-[95%] max-md:w-[47%] md:max-lg:w-[24%] pb-2 bg-white rounded-[20px] dark:dark:bg-[#29435c] dark:text-[#d1d4c9]">
+        <img src={image} className="w-[92%] h-36 md:max-lg:h-[42%] mx-auto border border-[gray] relative bottom-6 rounded-3xl"></img>
+        <h1 className="text-[14px] max-md:text-[12px] max-lg:text-[9px] font-bold pr-4 relative bottom-5 dark:text-[#d1d4c9]">{newsTitle}</h1>
+        <div className="relative flex justify-between bottom-3 pr-4 pl-3 max-md:text-[11px] text-[13px] md:max-lg:text-[10px] text-[#0004ff] dark:text-[#d1d4c9]">
+          <h6 className="text-red-700 dark:text-[#e19f9f]">{addUserFullName}</h6>
+          <div className='flex items-center'>
+            <span className='text-xs max-lg:text-[9px] pl-2'>{currentView}</span>
+            <img src={eye} className='w-5 max-lg:w-4 h-5 max-lg:h-4' alt=""></img>
+          </div>
         </div>
-    
-        <h1 className="pr-5 mt-2"> {title} </h1>
-        <p className=" mx-auto pr-3 mt-1 w-[95%] h-[50%] overflow-hidden"> {miniDescribe} </p>
+        <div className="flex gap-3">
+          <span className="relative bottom-2 bg-slate-400 mr-4 px-1 pt-1 pb-2 bg-opacity-60 rounded-md text-[12px] md:max-lg:text-[10px] dark:bg-opacity-50 dark:dark:bg-[#556e53]">{newsCatregoryName}</span>
+          <img src={isCurrentUserFavorite === true ? saved : save} className='w-5 max-lg:w-4 h-5 max-lg:h-4 cursor-pointer' alt=""></img>
+        </div>
+        <p className="w-[95%] indent-[8px] h-9 max-lg:h-6 max-md:h-8 pr-2 relative  max-sm:text-[11px]  max-md:text-[10px]  mx-auto text-[12px] md:max-lg:text-[8px]  overflow-hidden dark:text-[#d1d4c9]">{miniDescribe}</p>
         
-        <div className="flex relative bottom-20 mr-52 gap-2 "> 
+        <div className="flex justify-between mt-1 pl-3">
+        <div className="flex pr-4 gap-2">
+          <div className=''>
+            <img src={currentUserIsLike === true ? liked : notLiked} className='w-5 max-lg:w-4 h-5 max-lg:h-4 cursor-pointer' alt=""></img>
+            <span className='text-xs max-lg:text-[9px] mt-2  pr-[2px]'>{currentLikeCount}</span>
+          </div>
+          <div className=''>
+            <img src={currentUserIsDissLike === true ? disliked : notDisLiked} className='w-5 max-lg:w-4 h-5 max-lg:h-4 cursor-pointer' alt=""></img>
+            <span className='text-[12px] max-lg:text-[9px] mt-2  pr-[2px]'>{currentDissLikeCount}</span>
+          </div>
+        </div>
+          <Link to={'/NewsDetail/'+id} className="text-[12px] sm:text-[11px] h-9 pt-2 px-2 my-auto rounded-xl bg-slate-400 dark:dark:bg-[#556e53] dark:text-[#d1d4c9]">جزئیات خبر</Link>
+        </div>
+        <div  className="border-t border-slate-400 flex items-center justify-between w-[90%] mx-auto pt-2 max-lg:pt-1">
+            <span className="text-[13px] md:max-lg:text-[11px]"> تاریخ خبر</span>
+            <span className="   max-md:text-[12px] max-lg:text-[11px] text-[13px] max-xl:text-[13px] text-red-700 dark:text-[#d1d4c9]" >{insertDate}  </span>
+        </div>
+        {/* <span className="absolute mt-6 md:max-lg:mt-2 left-[2%] max-md:left-[3%]  max-md:text-[11px]  max-lg:pr-[4px] bg-slate-400 rounded-2xl text-[14px] max-xl:text-[13px] max-lg:text-[10px] p-[2px] pr-1 pl-5 md:max-lg:pl-4 bg-[url(public/timer.png)] bg-no-repeat [background-position:2%_50%] max-lg:[background-position:4%_50%] [background-size:18px] md:max-lg:[background-size:14px] dark:dark:bg-[#556e53] dark:text-[#d1d4c9]">{time}</span> */}
+        </div>
+    // <div className=" mt-3 bg-white rounded-lg w-[23%] dark:bg-[#152a38] max-lg:w-[45%]  max-sm:w-[100%] max-sm:mt-2 shadow-lg">
+        
+    //     <div className="h-[40%] w-[90%]  mx-auto pt-3 ">
+    //         <img src={image} alt="" className=" h-[100%] w-[100%] rounded-[20px]" />
+    //     </div>
+    
+    //     <h1 className="pr-5 mt-2"> {title} </h1>
+    //     <p className=" mx-auto pr-3 mt-1 w-[95%] h-[50%] overflow-hidden"> {miniDescribe} </p>
+        
+    //     <div className="flex relative bottom-20 mr-52 gap-2 "> 
             
-            <TiHeartOutline size={20} fill="red" /> 
+    //         <TiHeartOutline size={20} fill="red" /> 
     
-            <h1> {currentLikeCount}
-                <AiOutlineLike fill="" size={20} /> 
-            </h1>
+    //         <h1> {currentLikeCount}
+    //             <AiOutlineLike fill="" size={20} /> 
+    //         </h1>
 
-            <h1> {currentDissLikeCount}
-                <AiOutlineDislike size={20} /> 
-            </h1>
+    //         <h1> {currentDissLikeCount}
+    //             <AiOutlineDislike size={20} /> 
+    //         </h1>
 
-        </div>
+    //     </div>
 
-        <Link to={'/NewsDetail/'+id}>
-        <button className="btn  relative right-4 bottom-28 "> ادامه مطلب </button>
-        </Link>
+    //     <Link to={'/NewsDetail/'+id}>
+    //     <button className="btn  relative right-4 bottom-28 "> ادامه مطلب </button>
+    //     </Link>
 
-    </div>
+    // </div>
     
 
 );
