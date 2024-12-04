fb394c7b11eadbaa3daa5febfde53ddd6ec8bb90
oveiss <oveiss.ershadian@gmail.com>
2024-12-04 21:43:16 +0330

    .

--- src/screens/page news/TopSorting2.jsx
+++ src/screens/page news/TopSorting2.jsx
@@ -1,4 +1,4 @@
-import Sort2 from "./Sort2";
+
 
 function TopSorting2() {
   const data = [
@@ -8,43 +8,21 @@ function TopSorting2() {
   ];
   return (
     <div className="items-center md:flex mr-[25px] max-xl:mr-[5px] max-lg:mr-[15px]  ">
-      <ul className="flex item-center gap-4 max-lg:mr-4 dark:text-[#d1d4c9] max-sm:hidden ">
-        {data.map((item, index) => {
-          return (
-            <Sort2
-              key={index}
-              value={item.value}
-              id={item.lable}
-              htmlFor={item.lable}
-              topicSort={item.lable}
-            />
-          );
-        })}
-      </ul>
-      <div>
-        <details className="dropdown block sm:hidden max-sm:ml-10">
-          <summary className="btn m-1 max-sm:text-xs max-sm:w-[50px]">فیلتر </summary>
-          <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-56 p-2 shadow max-sm:w-[100px] max-sm:text-[10px]  whitespace-nowrap">
-            {data.map((item,index) =>{
-              return(
-                <li className="w-[90px]" key={index}>
-                  <input
-                    type="radio"
-                    value={item.value}
-                    id={item.lable}
-                    name="sort"
-                    className="hidden"
-                  />  
-                  <label htmlFor={item.lable}>
-                  {item.lable}
-                  </label>  
-                  
-                </li>
-              )
-            })}
-          </ul>
-        </details>
-      </div>
+      <details className="dropdown block  max-sm:ml-10">
+        <summary className="btn m-1 max-sm:text-xs max-sm:w-[50px]">فیلتر </summary>
+        <ul className="menu dropdown-content bg-base-100 max-sm:left-[0px] rounded-box z-[1] w-[200px] p-2 shadow    whitespace-nowrap">
+          {data.map((item,index) =>{
+            return(
+              <li className="" key={index}>
+                <label className="label cursor-pointer">
+                    <input type="radio" name="radio-2" value={item.value} id={item.lable} className="radio radio-primary " />
+                    <span className="label-text absolute right-14 text-[14px] md:max-lg:text-[12px] dark:text-[#d1d4c9]">{item.lable}</span>
+                </label>
+              </li>
+            )
+          })}
+        </ul>
+      </details>
     </div>
   );
 }
